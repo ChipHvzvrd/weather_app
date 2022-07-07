39c99fc29392f3a063c0083fd03231d70a945e45
Christian Pena <Chip@Christians-MBP.attlocal.net>
2022-07-07 17:18:05 -0500

    javascript for weather API

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,24 @@
-var button = document.querySelector('')
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var cityName = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+function locationApi(){
+
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=190273917e2fc4166132adb9a1518353')
+    .then(response => response.json())
+    //.then(data =>console.log(data))
+    .then(data => {
+        var nameValue = data['city']['name'];
+
+        cityName.innerHTML = nameValue;
+
+    })
+
+    //.catch(err => alert('Incorrect user input...'))
+}
+
+
+button.addEventListener('click', locationApi)
+
